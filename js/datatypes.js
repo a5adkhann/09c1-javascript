973621910e0ad0d6f9f07a38ab277dff03143ed5
amna001 <amnaaslam7789@gmail.com>
2025-11-12 12:57:54 +0500

    ---

---
+++ js/datatypes.js
@@ -0,0 +1,11 @@
+// console.log("Datatypes");
+
+/*
+Datatypes;
+1) String  -- "" or ''
+2) Number/Integer  -- numeric 
+3) Boolean  -- true/false
+4) Array  -- []
+5) Object  -- {}
+6) Function  -- ()
+*/
--- js/variable.js
+++ js/variable.js
@@ -1,9 +1,20 @@
-var name = "Ali";
+// var name;  // declaration
 
-let age = 22;
+// name = "Ali";  // initialization
 
-const email = "ali@gmail.com";
+
+// var
+// var name = "Ali";  // declaration + initialization
+// var name = "Ahmed";
+
+
+// let
+// let age = 22;
+// age = 24;
+
+
+// const
+const name = "Ali";
+name = "Ahmed";
 
 console.log(name);
-console.log(age);
-console.log(email);
