a7cf8469350d587a3b3bbff4cc4af5c4b3fe0de8
amna001 <amnaaslam7789@gmail.com>
2025-11-14 12:40:12 +0500

    ---

---
+++ js/builtInFunctions.js
@@ -0,0 +1,9 @@
+// alert("I am Mudassir");
+
+// let msg = prompt("Enter Your Name");
+
+// console.log(msg);
+
+let confirmation = confirm("You really want to delete this item???");
+
+console.log(confirmation);
--- js/datatypes.js
+++ js/datatypes.js
@@ -9,3 +9,63 @@ Datatypes;
 5) Object  -- {}
 6) Function  -- ()
 */
+
+var name = "Mudassir"; // String
+var age = 22;  // Integer
+let enrolled = true;  // Boolean
+let courses = ["ADSE", "ENG"];  // Array
+
+let address = {   // Object
+    street: "123 Main St.",
+    city: "Karachi",
+    zip: 2345
+}
+
+let info = {
+    street: {
+        city: "Karachi"
+    }
+}
+
+console.log(info.street.city);
+
+// execution
+console.log(name);  // string
+console.log(age);   // integer
+console.log(enrolled);  // boolean
+console.log(courses[1]);  // array index 1
+console.log(courses[0]);  // array index 0
+console.log(address.street);  // object prop street
+console.log(address.city);  // object prop city
+console.log(address.zip);  // object prop zip
+
+
+/*
+- There are two types of custom functions.
+1) Basic/Regular Function
+2) Arrow Function
+*/
+
+// function verification(a, b){
+//     console.log("You are Verified!!!");
+//     console.log(a+b);
+// }
+
+// verification(2, 2);
+// verification(20, 25);
+// verification(45, 10);
+
+abc = () => {
+    console.log("You are Verified!!!");
+}
+
+abc();  
+
+
+
+
+
+
+
+
+
